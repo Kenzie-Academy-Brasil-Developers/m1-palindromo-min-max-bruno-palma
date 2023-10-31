7526d5ad0e3749d4eb8b1315e62bfb7b89bcd881
bruno-palma <brunopalma1125@gmail.com>
2023-10-31 12:36:28 -0300

    Tarefa 1 - Testes de Palíndromos em JS

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+  let texto = str.toLowerCase();
+  let textoInverso = "";
+  for (let i = texto.length; i > 0; i--) {
+    textoInverso += texto[i - 1];
+  }
+  if (texto.replaceAll(" ", "") === textoInverso.replaceAll(" ", "")) {
+    return true;
+  }
+  return false;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  /* Seu código aqui */
 }
